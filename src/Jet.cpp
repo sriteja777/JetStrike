87ef59fc2d1dcfcb9ab0f5e550e505b206af8b44
Sriteja Sugoor <sritejakittu777@gmail.com>
2019-05-06 17:15:02 +0530

    Added bullet launchers for jet

--- src/Jet.cpp
+++ src/Jet.cpp
@@ -4,8 +4,11 @@
 
 #include "Jet.h"
 #include "Camera.h"
+#include "Compass.h"
 
 extern Camera cam;
+extern Compass compass;
+extern float ocean_surface;
 Jet::Jet(glm::vec3 position) {
     this->position = position;
 
@@ -13,6 +16,17 @@ Jet::Jet(glm::vec3 position) {
     float fuselage_radius = 0.25;
     float cockpit_length = 0.5;
     float tail_length = 0.5;
+    float bullet_launcher_distance = 1;
+    float bullet_laucher_radius = 0.05;
+    float bullet_launcher_length = 0.2;
+
+    this->altitude = (position.y - ocean_surface) * 10;
+    this->fuel = 1000;
+    printf("Altitude, fuel -> %f, %f", altitude, fuel);
+//    exit(0);
+
+    this->dis_lbl = glm::vec3(bullet_launcher_distance,bullet_laucher_radius,0.4);
+    this->dis_rbl = glm::vec3(-bullet_launcher_distance,bullet_laucher_radius,0.4);
 
     jet_axis = glm::vec3(0, 0, -1);
     jet_per_axis = glm::vec3(1,0,0);
@@ -25,21 +39,23 @@ Jet::Jet(glm::vec3 position) {
             -0.2f, -0.15f, 0.0f,
     };
 
+    glm::vec3 scale_values = {0.25, 0.25, 0.25};
 
+    this->fuselage = Cylinder(position, fuselage_length, fuselage_radius, COLOR_RUST);
 
-    this->fuselage = Cylinder(position.x, position.y, position.z, fuselage_length, fuselage_radius, COLOR_RUST);
-
-    this->cockpit = Cone(glm::vec3(position.x, position.y, position.z - cockpit_length), cockpit_length, fuselage_radius, COLOR_RUST, 0);
+    this->cockpit = Cone(glm::vec3(position.x, position.y, position.z - cockpit_length), cockpit_length, fuselage_radius, COLOR_RUST, 0, {0,0,1});
     this->tail = ObliqueCone(glm::vec3(position.x, position.y + fuselage_radius, position.z + fuselage_length + tail_length), position, tail_length, fuselage_radius, COLOR_RUST, 180, glm::vec3(0,1,0));
 
     this->left_wing = TwoDPolygon(4, wing_coordinates, glm::vec3(position.x - fuselage_radius, position.y, position.z),
                                   COLOR_DARK_ORANGE, glm::vec3(6.0f, 4.0f, 0.0f), -90.0f, glm::vec3(1, 0, 0));
     this->right_wing = TwoDPolygon(4, wing_coordinates, glm::vec3(position.x + fuselage_radius, position.y, position.z),
                                   COLOR_DARK_ORANGE, glm::vec3(6.0f, 4.0f, 0.0f), -90.0f, glm::vec3(1, 0, 0));
-    right_wing.reflect(glm::vec3());
+    right_wing.reflect(Y_AXIS);
     this->left_horizontal_stabiliser = TwoDPolygon(4, wing_coordinates, glm::vec3(position.x, position.y+0.15, position.z + fuselage_length+0.1), COLOR_DARK_ORANGE, glm::vec3(3, 2, 1), -90, glm::vec3(1,0,0));
     this->right_horizontal_stabiliser = TwoDPolygon(4, wing_coordinates, glm::vec3(position.x, position.y+0.15, position.z + fuselage_length+0.1), COLOR_DARK_ORANGE, glm::vec3(3, 2, 1), -90, glm::vec3(1,0,0));
-    right_horizontal_stabiliser.reflect(glm::vec3());
+    right_horizontal_stabiliser.reflect(Y_AXIS);
+
+
     GLfloat ver_coordinates[] = {
             0.0f, 0.0f, 0.0f,
             0.0f, 0.0f, 0.1f,
@@ -51,8 +67,19 @@ Jet::Jet(glm::vec3 position) {
 
     this->vertical_stabiliser = TwoDPolygon(4, ver_coordinates, glm::vec3(position.x, position.y+fuselage_radius, position.z + fuselage_length+0.1), COLOR_DARK_ORANGE, glm::vec3(1, 3, 2), 2, glm::vec3(0,0,1));
 
+    this->left_bullet_launcher = Cylinder(glm::vec3(position.x - bullet_launcher_distance, position.y - bullet_laucher_radius, position.z + dis_lbl.z),
+                                          bullet_launcher_length, bullet_laucher_radius, COLOR_SADDLE_BROWN);
+
+    this->right_bullet_launcher = Cylinder(glm::vec3(position.x + bullet_launcher_distance, position.y - bullet_laucher_radius, position.z + dis_lbl.z),
+                                          bullet_launcher_length, bullet_laucher_radius, COLOR_SADDLE_BROWN);
+
     this->position = glm::vec3(position.x, position.y, position.z + fuselage_length/2);
     //    exit(0);
+
+    barrel_rolling = false;
+    barrel_radius = 2;
+    barrel_angle = 0;
+
 }
 
 void Jet::draw(glm::mat4 VP) {
@@ -65,6 +92,8 @@ void Jet::draw(glm::mat4 VP) {
     this->right_horizontal_stabiliser.draw(VP, true);
     this->tail.draw(VP);
     this->vertical_stabiliser.draw(VP);
+    this->left_bullet_launcher.draw(VP);
+    this->right_bullet_launcher.draw(VP);
 }
 
 void Jet::translate(glm::vec3 diff) {
@@ -77,6 +106,10 @@ void Jet::translate(glm::vec3 diff) {
     this->left_horizontal_stabiliser.update_position(diff);
     this->right_horizontal_stabiliser.update_position(diff);
     this->vertical_stabiliser.update_position(diff);
+    this->left_bullet_launcher.update_position(diff);
+    this->right_bullet_launcher.update_position(diff);
+
+    fuel -= glm::length(diff)/1.5;
 //    cam.eye.x += diff.x;
 //    cam.eye.y += diff.y;
 //    cam.eye.z += diff.z;
@@ -98,23 +131,19 @@ void Jet::update_position_y(float y) {
     glm::vec3 diff = glm::vec3(0,y,0);
     this->position.y += y;
     this->translate(diff);
+    this->altitude += y*10;
     //    cam.eye.y -= y;
 //    cam.target.y += y;
 }
 void Jet::update_position_z(float z) {
     glm::vec3 diff = glm::vec3(0,0,z);
     this->position.z += z;
+
     this->translate(diff);
 }
 
 void Jet::update_rotation(float rotate) {
 //    this->body.rotation += rotate;
-}
-
-
-
-void Jet::move_left() {
-
 }
 
 
@@ -127,11 +156,19 @@ void Jet::rotate(float angle, glm::vec3 axis) {
     this->right_horizontal_stabiliser.rotate(angle, glm::vec3(0, -0.15, -fuselage.length/2-0.1), axis);
     this->vertical_stabiliser.rotate(angle, glm::vec3(0, -fuselage.radius, -fuselage.length/2-0.1), axis);
     this->tail.rotate(angle, glm::vec3(0, -fuselage.radius, -tail.length - fuselage.length/2)  ,axis);
+    this->left_bullet_launcher.rotate(angle, glm::vec3(dis_lbl.x, left_bullet_launcher.radius, fuselage.length/2-dis_lbl.z), axis);
+    this->right_bullet_launcher.rotate(angle, glm::vec3(dis_rbl.x, dis_lbl.y, fuselage.length/2 - dis_rbl.z), axis);
 
     glm::mat4 rot = glm::rotate(glm::radians(angle), axis);
-    jet_axis = rot * glm::vec4 (jet_axis.x, jet_axis.y, jet_axis.z, 1);
-    jet_per_axis = rot * glm::vec4(jet_per_axis.x, jet_per_axis.y, jet_per_axis.z, 1);
-    printf("\naxis -> %f %f %f\n", jet_axis.x, jet_axis.y, jet_axis.z);
+    if (DEBUG) print_mat4(rot);
+    if (DEBUG) printf("\nbefore axis -> %f %f %f\n", jet_axis.x, jet_axis.y, jet_axis.z);
+    jet_axis = glm::normalize(rot * glm::vec4 (jet_axis.x, jet_axis.y, jet_axis.z, 0));
+    jet_per_axis = glm::normalize(rot * glm::vec4(jet_per_axis.x, jet_per_axis.y, jet_per_axis.z, 0));
+    if (DEBUG) printf("\naxis -> %f %f %f\n", jet_axis.x, jet_axis.y, jet_axis.z);
+//    exit(0);
+
+    //Rotate Compass
+
 
 }
 
@@ -139,27 +176,50 @@ void Jet::tick() {
 //    update_position_z(-0.05f);
 //    cam.eye.z -=0.05f;
 //    cam.target.z -=0.05f;
-    float s = 0.01f;
-    printf("\naxis -> %f %f %f\n", jet_axis.x, jet_axis.y, jet_axis.z);
+    float s = 0.2f;
+    if (DEBUG) printf("\naxis -> %f %f %f\n", jet_axis.x, jet_axis.y, jet_axis.z);
+
 //    if (jet_axis.y < 0) {
 //        exit(0);
 //    }
+    fuel -= 0.05;
+    if (barrel_rolling) {
+        barrel_roll();
+    }
 //    update_position_x(jet_axis.x*s);
 //    update_position_y(jet_axis.y*s);
 //    update_position_z(jet_axis.z*s);
 
 
+
 }
 
-void Jet::roll(int sign) {
-    float angle = -sign*0.5f;
+
+void Jet::barrel_roll() {
+    if (barrel_angle >= 360) {
+        barrel_rolling = false;
+        barrel_angle = 0;
+        return;
+    }
+    this->update_position_x(barrel_radius * std::cos(glm::radians(barrel_angle)));
+    this->update_position_y(barrel_radius * std::sin(glm::radians(barrel_angle)));
+    roll(1, 5);
+    barrel_angle += 5;
+
+}
+
+
+
+
+void Jet::roll(int sign, float angle) {
+    angle *= -sign;
     glm::vec3 axis = jet_axis;
     this->rotate(angle, axis);
 
 }
 
-void Jet::pitch(int sign) {
-    float angle = sign*0.5f;
+void Jet::pitch(int sign, float angle) {
+    angle *= sign;
     glm::vec3 axis = jet_per_axis;
     this->rotate(angle, axis);
 //    float speed = 0.05f;
@@ -170,16 +230,28 @@ void Jet::pitch(int sign) {
 
 }
 
-void Jet::yaw(int sign) {
-    float angle = sign*0.5f;
+void Jet::yaw(int sign, float angle) {
+    angle *= sign;
     glm::vec3 axis = glm::cross(jet_per_axis, jet_axis);
     this->rotate(angle, axis);
+    compass.rotate(angle);
 }
 
 void Jet::move_front() {
-    float speed = 0.05f;
+    float speed = 0.5f;
     update_position_x(jet_axis.x*speed);
     update_position_y(jet_axis.y*speed);
     update_position_z(jet_axis.z*speed);
 
+}
+
+void Jet::move_right() {
+
+    yaw(-1, 0.5);
+    roll(-1, 0.25);
+}
+
+void Jet::move_left() {
+    yaw(1, 0.5);
+    roll(1, 0.25);
 }
--- src/Jet.h
+++ src/Jet.h
@@ -17,7 +17,7 @@ public:
     Jet(glm::vec3 position);
     void draw(glm::mat4 VP);
     glm::vec3 position;
-
+    glm::vec3 rotation;
     Cylinder fuselage;
     Cone cockpit;
     ObliqueCone tail;
@@ -26,11 +26,16 @@ public:
     TwoDPolygon right_horizontal_stabiliser;
     TwoDPolygon vertical_stabiliser;
     TwoDPolygon right_wing;
+    Cylinder left_bullet_launcher;
+    Cylinder right_bullet_launcher;
 
     glm::vec3 velocity;
     glm::vec3 acceleration;
     glm::vec3 jet_axis;
     glm::vec3 jet_per_axis;
+
+    float altitude;
+    float fuel;
 
     void update_position_x(float x);
     void update_position_y(float y);
@@ -38,12 +43,14 @@ public:
     void update_rotation(float rotate);
     void move_left();
     void move_front();
-    void yaw(int sign);
-    void roll(int sign);
-    void pitch(int sign);
+    void move_right();
+    void yaw(int sign, float angle=2.f);
+    void roll(int sign, float angle=2.f);
+    void pitch(int sign, float angle=2.f);
     void tick();
     void rotate(float angle, glm::vec3 axis);
     void translate(glm::vec3 diff);
+
     // Displacements
 
     //    glm::vec3 dis_cockpit;
@@ -54,6 +61,14 @@ public:
     //    glm::vec3 dis_right_horizontal_stab;
     //    glm::vec3 dis_vertical_stab;
     //    glm::vec3 dis_tail;
+    glm::vec3 dis_lbl;
+    glm::vec3 dis_rbl;
+
+
+    bool barrel_rolling;
+    float barrel_angle;
+    float barrel_radius;
+    void barrel_roll();
 };
 
 
