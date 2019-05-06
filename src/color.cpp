6f6a66b99b496e78b78bbe1f2ef519e81c9552ec
Sriteja Sugoor <sritejakittu777@gmail.com>
2019-05-06 17:10:55 +0530

    Added new colors

--- src/color.cpp
+++ src/color.cpp
@@ -1,9 +1,10 @@
 #include "main.h"
 
 const color_t COLOR_RED = { 255, 0, 0 };
-const color_t COLOR_GREEN = { 0,255,0 };
+const color_t COLOR_LIME = { 0,255,0 };
 const color_t COLOR_BLUE = {0,0,255};
-const color_t COLOR_BLACK = { 52, 73, 94 };
+const color_t COLOR_GREEN = {0,128,0};
+const color_t COLOR_BLACK = { 0,0,0 };
 const color_t COLOR_BACKGROUND = { 242, 241, 239 };
 const color_t COLOR_DARK_TURQUOISE = {0,206,209};
 const color_t COLOR_SKY_BLUE = {135,206,235};
@@ -22,3 +23,12 @@ const color_t COLOR_ORANGE_RED = {255, 69, 0};
 const color_t COLOR_SILVER = {192,192,192};
 const color_t COLOR_ORCHID = {218,112,214};
 const color_t COLOR_RUST = {150,77,5};
+const color_t COLOR_YELLOW = {255, 255, 0};
+const color_t COLOR_WHITE = {255,255,255};
+const color_t COLOR_MAGNETA = {255,0, 255};
+const color_t COLOR_THISTLE = {216,191,216};
+const color_t COLOR_SANDY_BROWN = {244, 164, 96};
+const color_t COLOR_DEEP_PINK = {255, 20, 147};
+const color_t COLOR_DIM_GREY = {105, 150, 105};
+const color_t COLOR_GAINSBORO = {220, 220, 220};
+
