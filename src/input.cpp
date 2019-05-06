4809680bdac3efb6b6f053f0e65be8c8ad6e2f89
Sriteja Sugoor <sritejakittu777@gmail.com>
2019-05-06 17:17:43 +0530

    Added code for keyboard and mouse inputs for controlling camera views

--- src/input.cpp
+++ src/input.cpp
@@ -15,8 +15,16 @@
 #include "main.h"
 #include "Jet.h"
 #include "Camera.h"
+#include "Bullet.h"
+#include "Bomb.h"
+#include "Canon.h"
+#include "Parachute.h"
 extern Jet player;
 extern Camera cam;
+extern std::vector<Bullet> bullets;
+extern Canon enemy;
+extern std::vector<Bomb> bombs;
+extern std::vector<Parachute> parachute_enemies;
 double old_x = 1024/2;
 double old_y = 768/2;
 
@@ -24,6 +32,7 @@ bool   cannon_keyboard_input = true;
 bool   drag_pan = false, old_cki;
 double drag_oldx = -1, drag_oldy = -1;
 float rotation_val =0;
+
 
 using namespace std;
 
@@ -64,6 +73,29 @@ void keyboard(GLFWwindow *window, int key, int scancode, int action, int mods) {
             case GLFW_KEY_D:
                 player.roll(+1);
                 break;
+            case GLFW_KEY_F1:
+                cam.change_view(plane);
+                break;
+            case GLFW_KEY_F2:
+                cam.change_view(top);
+                break;
+            case GLFW_KEY_F3:
+                cam.change_view(tower);
+                break;
+            case GLFW_KEY_F4:
+                cam.change_view(follow);
+                break;
+            case GLFW_KEY_F5:
+                cam.change_view(helicopter);
+                break;
+            case GLFW_KEY_M:
+                enemy.shoot(player.position);
+                break;
+            case GLFW_KEY_P:
+                parachute_enemies[0].shoot(player.position);
+                break;
+            case GLFW_KEY_B:
+                player.barrel_rolling = true;
             default:
                 break;
         }
@@ -90,14 +122,15 @@ void mouseButton(GLFWwindow *window, int button, int action, int mods) {
                 // Do something
                 return;
             } else if (action == GLFW_RELEASE) {
+                bullets.emplace_back(Bullet(player));
                 // Do something
             }
             break;
-            // case GLFW_MOUSE_BUTTON_RIGHT:
-            // if (action == GLFW_RELEASE) {
-            // rectangle_rot_dir *= -1;
-            // }
-            // break;
+             case GLFW_MOUSE_BUTTON_RIGHT:
+             if (action == GLFW_PRESS) {
+                    bombs.emplace_back(Bomb(player));
+             }
+             break;
         default:
             break;
     }
@@ -110,7 +143,7 @@ void scroll_callback(GLFWwindow *window, double xoffset, double yoffset) {
     else if (yoffset > 0) {
         zoom_angle += 1;
     }
-    printf("zoom angle -> %f\n",zoom_angle);
+    if (DEBUG) printf("zoom angle -> %f\n",zoom_angle);
     reset_screen();
     // Do something
 }
@@ -119,13 +152,25 @@ void cursor_pos_callback(GLFWwindow *window, double xpos, double ypos) {
 //    if (xpos > 1600 || xpos < -520) {
 //        return;
 //    }
-    double xoffset = xpos - old_x;
-    double yoffset = ypos - old_y;
-    player.update_position_x(static_cast<float>(xoffset/500));
-    player.update_position_y(static_cast<float>(-yoffset/500));
+
+//    printf("%f %f\n", xpos, ypos);
+
+//    if ((xpos < 200 || xpos > 1000) && (ypos < 100 || ypos > 600)) {
+//        if (xpos > 1000) {
+//            cam.hel_rotation.x += 1;
+//        }
+//        if (xpos < 200) {
+//            cam.hel_rotation.x -= 1;
+//        }
+//    }
+
+//    double xoffset = xpos - old_x;
+//    double yoffset = ypos - old_y;
+//    player.update_position_x(static_cast<float>(xoffset/500));
+//    player.update_position_y(static_cast<float>(-yoffset/500));
 //    double rot = atan(xoffset/yoffset);
 //    player.update_rotation(static_cast<float>(rot));
-    old_x = xpos;
-    old_y = ypos;
-    printf("xpos -> %lf, ypos-> %lf\n",xpos, ypos);
+//    old_x = xpos;
+//    old_y = ypos;
+    if (DEBUG) printf("xpos -> %lf, ypos-> %lf\n",xpos, ypos);
 }
